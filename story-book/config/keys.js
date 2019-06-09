3435a8ce5c3640861322c1973c45d59d1371c8fb
Carlos Hudson <camihudsondiaz@ittepic.edu.mx>
2019-06-08 22:01:22 -0500

    41. Creating our google strategy

---
+++ story-book/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://hudson21:flute5816@ds235197.mlab.com:35197/storybooks-dev',
+    googleCLientID: '848721059525-va20i5hipqpqaa6sl8ck5bk04h9vs3ub.apps.googleusercontent.com',
+    googleClientSecret: 'muYQ553H1SFrgRWYDp6lbO4r'
+};
