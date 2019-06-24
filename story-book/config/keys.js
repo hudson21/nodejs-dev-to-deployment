594a20ad2c8105068a20c47f1503add446aa1f67
hudson21 <camihudsondiaz@ittepic.edu.mx>
2019-06-23 22:01:49 -0500

    Finish the course and the app is in production :)

--- story-book/config/keys.js
+++ story-book/config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://hudson21:flute5816@ds235197.mlab.com:35197/storybooks-dev',
-    mongoURILocalhost: 'mongodb://localhost/storybooks-dev',
-    googleCLientID: '18724321230-q0jd94jhdaqgsnao81qoiofa9skdvk8n.apps.googleusercontent.com',
-    googleClientSecret: '2L9JnKPHgWTQfBVNrlrC0d8f'
-};
-
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
